2a921fd45c0eed7d30944c9857b6f5582cc906a9
kliberty <8992170+kliberty@users.noreply.github.com>
2021-11-06 12:07:07 -0400

    Add a special indicator to the version name

--- weed/util/constants.go
+++ weed/util/constants.go
@@ -8,8 +8,9 @@ var (
 	VERSION_NUMBER = fmt.Sprintf("%.02f", 2.76)
 	VERSION        = sizeLimit + " " + VERSION_NUMBER
 	COMMIT         = ""
+	SPECIAL        = "Image Cache"
 )
 
 func Version() string {
-	return VERSION + " " + COMMIT
+	return VERSION + " " + COMMIT + " " + SPECIAL
 }
