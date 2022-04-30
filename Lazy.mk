0a25d4e189e5e7285c66e85d006a92753bb8ba2f
kliberty <8992170+kliberty@users.noreply.github.com>
2022-04-30 11:32:12 -0400

    add debugging

--- Lazy.mk
+++ Lazy.mk
@@ -1,7 +1,10 @@
-.PHONY: weed-large-ic-pg-mqtt-json weed-ic
+.PHONY: weed-large-ic-pg-mqtt-json weed-ic weed-large-ic-pg-json-mqtt-debug
 
 weed-large-ic-pg-json-mqtt:
 	cd weed && go build -tags 5BytesOffset -o $@
+	
+weed-large-ic-pg-json-mqtt-debug:
+	cd weed && go build -tags 5BytesOffset -gcflags=all="-N -l" -o $@
 
 weed-ic:
 	go build -o $@
